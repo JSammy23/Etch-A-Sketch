60c5ede3abebcef6d4256e7ccd2941fad808130e
Jordan Sampson <jordansampson23@gmail.com>
2022-11-30 14:03:39 -0600

    Grid function 16x16 in grid-cont

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const newDiv = document.createElement('div')
 
 
 function createGrid() {
-    for (var i = 0; i < 16; i++){
+    for (var i = 0; i < 256; i++){
         grid.innerHTML += '<div class="box"></div>';
     }
 }
 
-createGrid();
+
 
